930335851ee30bde3f22c9d0da2a425702fd7288
ashish sahoo <119804958+ashish123-code@users.noreply.github.com>
2023-02-11 21:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+  const btn = document.querySelector("#btn");
+  const num = document.querySelector("#num");
 
+  btn.addEventListener("click", function() {
+    let randomNum = Math.floor(Math.random() * 41) - 20;
+    num.textContent = randomNum;
+  });
