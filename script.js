1bd90d0ce722838c71eebecd253085014afb8354
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-02-14 18:17:24 +0530

    fgh

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //your code here
-  const btn = document.querySelector("#btn");
-  const num = document.querySelector("#num");
+var randomNum;
+var input;
 
-  btn.addEventListener("click", function() {
-    let randomNum = Math.floor(Math.random() * 41) - 20;
-    num.textContent = randomNum;
-  });
+function randomNumGenerator() {
+  randomNum = Math.floor(Math.random() * 41) - 20;
+  document.getElementById("num").innerText = randomNum;
+  guessNum();
+  print();
+}
+
+function guessNum() {
+  input = Number(document.getElementById("guess").value);
+}
+function print() {
+  if (Math.abs(randomNum - input) <= 5) {
+    document.getElementById("respond").innerText = "Hot";
+  } else {
+    document.getElementById("respond").innerHTML = "Cold";
+  }
+}
